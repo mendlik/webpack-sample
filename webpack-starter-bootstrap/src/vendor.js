5fe0aa48ba66877f46d46f8c538954bc3410bb20
mendlik <mendelski.pawel@gmail.com>
2017-11-03 15:12:55 +0100

    Improve vendor.js

--- webpack-starter-bootstrap/src/vendor.js
+++ webpack-starter-bootstrap/src/vendor.js
@@ -1,17 +1,12 @@
 import 'babel-polyfill';
 
-// Theme dependencies
-const loadTheme = () => {
-  // Issue: https://github.com/Dogfalo/materialize/issues/3812
-  // eslint-disable-next-line
-  const jquery = require('jquery');
-  // eslint-disable-next-line
-  const popper = require('popper.js');
-  global.$ = jquery;
-  global.jQuery = jquery;
-  global.Popper = popper;
-  // eslint-disable-next-line
-  require('bootstrap');
-};
-
-loadTheme();
+// Issue: https://github.com/Dogfalo/materialize/issues/3812
+// eslint-disable-next-line
+const jquery = require('jquery/dist/jquery.slim');
+// eslint-disable-next-line
+const popper = require('popper.js');
+global.$ = jquery;
+global.jQuery = jquery;
+global.Popper = popper;
+// eslint-disable-next-line
+require('bootstrap');
